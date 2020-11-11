dd69df44ee6f845714748de60bc72b4745b2be9f
Riey <creeper844@gmail.com>
2020-11-12 02:13:21 +0900

    Remove unused files

--- examples/add/worker-pre.js
+++
@@ -1,4 +0,0 @@
-function ws_worker_new() {
-    let worker = new Worker("worker.js", {name: self.toString()});
-    return worker;
-}
--- examples/add/worker.js
+++
@@ -1,8 +0,0 @@
-self.onmessage = async function(event) {
-    const [module, memory, entry, scripts] = event.data;
-    importScripts(scripts);
-    await wasm_bindgen(module, memory);
-    const spawn_entry = wasm_bindgen.SpawnEntry.__wrap(entry.ptr);
-    wasm_bindgen.ws_thread_entry(spawn_entry);
-    close();
-};
